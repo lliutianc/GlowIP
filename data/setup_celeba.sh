e4a30c00fb839b54533852df9aeabb8ca834cbe1
lliutianc <lliuitanc@gmail.com>
2021-05-18 20:25:01 -0400

    fix celeba download

--- data/setup_celeba.sh
+++ data/setup_celeba.sh
@@ -1,2 +1,2 @@
-wget https://storage.googleapis.com/glow-demo/data/celeba-tfr.tar -P ./celeba/
+wget https://openaipublic.azureedge.net/glow-demo/data/celeba-tfr.tar -P ./celeba/
 tar -C ./celeba/ -zxvf ./celeba/celeba-tfr.tar
