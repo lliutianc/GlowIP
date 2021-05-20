ba72b6f210c371ab9df52aa6df9c871dc89e49c3
lliutianc <lliuitanc@gmail.com>
2021-05-20 10:25:09 -0400

    add param to customize cuda

--- train_glow.py
+++ train_glow.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.cuda)
 
     print(f'Prepare: check process on cuda: {args.cuda}...')
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
