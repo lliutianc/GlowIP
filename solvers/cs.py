8bd3241a7c7560b3734f9459b63b01477e04ccbe
lliutianc <lliutianc@gmail.com>
2021-05-23 17:34:57 -0400

    try handling other gpu issue

--- solvers/cs.py
+++ solvers/cs.py
@@ -92,10 +92,10 @@ def GlowCS(args):
                         coupling=configs["coupling"],
                         n_bits_x=configs["n_bits_x"],
                         nn_init_last_zeros=configs["last_zeros"],
-                        device=args.device)
+                        device=args.device).to(args.device)
             glow.load_state_dict(torch.load(modeldir+"/glowmodel.pt", map_location=args.device))
             glow.eval()
-
+            print(glow.device)
             # making a forward to record shapes of z's for reverse pass
             _ = glow(glow.preprocess(torch.zeros_like(x_test)))
             
