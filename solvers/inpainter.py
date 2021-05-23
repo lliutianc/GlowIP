ce825651111b86bdab6f4d5142dfd82ad7e8a02b
lliutianc <lliutianc@gmail.com>
2021-05-23 18:02:14 -0400

    not empty cache

--- solvers/inpainter.py
+++ solvers/inpainter.py
@@ -200,7 +200,7 @@ def GlowInpaint(args):
             glow.zero_grad()
             optimizer.zero_grad()
             del x_test, x_gen, optimizer, psnr_t, z_sampled, glow, mask,
-            torch.cuda.empty_cache()
+            # torch.cuda.empty_cache()
             print("\nbatch completed")
         
         if skip_to_next:
