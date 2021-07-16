1e0c6c3fb2a550fa3896f3c5379fbcd44b2e0c89
lliutianc <lliutianc@gmail.com>
2021-07-15 20:37:11 -0400

    save train strate to result

--- solvers/newdenoiser.py
+++ solvers/newdenoiser.py
@@ -457,6 +457,7 @@ def GlowDenoiser(args):
             save_path = os.path.join(save_path, f'{args.noise}_'
                                                 f'{args.noise_loc}#{args.noise_scale}_'
                                                 f'{args.noise_channel}_{args.noise_area}_'
+                                                f'{args.train_strategy}_'
                                                 f'{args.init_strategy}_{augs}_'
                                                 f'{round(gamma, 4)}_{gettime()}')
             save_path = save_path.replace('__', '_')
